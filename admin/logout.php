729699a8190c2a49a6162bc35da66de75aa740ec
simon <simonwubshet254@gmail.com>
2024-07-12 14:35:08 +0300

    logout session edited

--- admin/logout.php
+++ admin/logout.php
@@ -5,5 +5,5 @@ unset($_SESSION['ADMIN_IS_LOGIN']);
 unset($_SESSION['ADMIN_USER']);
 unset($_SESSION['ADMIN_USER_NAME']);
 unset($_SESSION['ADMIN_ID']);
-redirect('login.php');
+redirect('index.php');
 ?>
