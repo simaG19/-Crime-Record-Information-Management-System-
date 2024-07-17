cc8994ae7602feb86e101ac766cd4bf1520431bf
simon <simonwubshet254@gmail.com>
2024-07-17 19:55:58 +0300

    error fixed

--- admin/manage_category.php
+++ admin/manage_category.php
@@ -12,7 +12,7 @@ if(isset($_GET['id']) && $_GET['id']>0){
 
 if(isset($_POST['submit'])){
 	$category=get_safe_value($con,$_POST['category']);
-	$address=get_safe_value($con,$_POST['address']);
+	
 	
 	if($id==''){
 		$sql="select * from category where category='$category'";
